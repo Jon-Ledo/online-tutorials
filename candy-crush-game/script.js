b56790bc6e3c582a9bf3810fe61c9ec08e9392d4
Jon-Ledo <jonledo.code@gmail.com>
2021-05-11 23:56:06 -0400

    draggable styles

--- candy-crush-game/script.js
+++ candy-crush-game/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   const candyColours = [
-    'thistle',
+    'blue',
     'rebeccapurple',
     'red',
     'orange',
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
 
   // Create Board
-  createBoard = () => {
+  const createBoard = () => {
     for (let i = 0; i < width * width; i++) {
       // for loop to create the 8x8 grid => flexwrap will ensure they wrap around
 
@@ -23,12 +23,64 @@ document.addEventListener('DOMContentLoaded', () => {
       const square = document.createElement('div');
       // generate random variable, to assign a colour to each square
       let randomColour = Math.floor(Math.random() * candyColours.length);
-      square.style.backgroundColor = candyColours[randomColour]
+      square.style.backgroundColor = candyColours[randomColour];
+      // Make each square on the screen draggable by using the attribute DRAGGABLE
+      square.setAttribute('draggable', true);
+      // give an ID to each square, so when you move/drag one we know which one is beingmoved
+      square.setAttribute('id', i)
+
       grid.appendChild(square);
       squares.push(square);
     }
   }
   createBoard();
+
+  // Drag the Candies
+  squares.forEach(square => square.addEventListener('dragstart', dragStart));
+  squares.forEach(square => square.addEventListener('dragend', dragEnd));
+  squares.forEach(square => square.addEventListener('dragover', dragOver));
+  squares.forEach(square => square.addEventListener('dragenter', dragEnter));
+  squares.forEach(square => square.addEventListener('dragleave', dragLeave));
+  squares.forEach(square => square.addEventListener('drop', dragDrop));
+
+  function dragStart() {
+    console.log(this.id, 'dragstart');
+  }
+
+  function dragOver() {
+    console.log(this.id, 'dragover');
+  }
+
+  function dragEnter() {
+    console.log(this.id, 'dragenter');
+  }
+
+  function dragLeave() {
+    console.log(this.id, 'dragleave');
+  }
+
+  function dragEnd() {
+    console.log(this.id, 'dragend');
+  }
+
+  function dragDrop() {
+    console.log(this.id, 'dragdrop');
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
